77cac96490af99e30a1fe7eb5e1ef74d12efe6a1
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-18 10:49:00 +0800

    rename variable to blackCardType

--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class Poker {
     public String compareResult(String blackPlayerCard, String whitePlayerCard) {
         String winResult = "";
-        String blackType = judgeType(blackPlayerCard);
-        String whiteType = judgeType(whitePlayerCard);
+        String blackCardType = getCardType(blackPlayerCard);
+        String whiteCardType = getCardType(whitePlayerCard);
         String[] type = {"StraightFlush", "FourOfAKind", "FullHouse", "Flush", "Straight", "ThreeOfAKind", "TwoPair", "OnePair", "HighCard"};
         int[] blackNumber = strNumber(blackPlayerCard);
         int[] whiteNumber = strNumber(whitePlayerCard);
-        int blackIndex = judgeIndex(blackType);
-        int whiteIndex = judgeIndex(whiteType);
+        int blackIndex = judgeIndex(blackCardType);
+        int whiteIndex = judgeIndex(whiteCardType);
         int[] blackArraySort = arraySort(blackNumber);
         int[] whiteArraySort = arraySort(whiteNumber);
         int[] blackRepeat = noOrRepeatNumber(blackNumber, 0);
@@ -247,7 +247,7 @@ public class Poker {
     }
 
     //判断是什么牌
-    private String judgeType(String str) {
+    private String getCardType(String str) {
         String type = "";
         String[] strArray = str.split("");
         int[] number = strNumber(str);
